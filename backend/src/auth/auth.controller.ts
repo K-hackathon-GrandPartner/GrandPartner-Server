11b7ec04d6942e5cde668d952ca7f97e1a714136
Jang-DongHo <donghoo18@naver.com>
2023-09-12 20:44:01 +0900

    [Fix] 로그인 Swagger 출력 값 변경

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginRequestDto } from './dto/login.dto';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import {
+  LoginRequestDto,
+  LoginResponseDto,
+  NotExistUserResponseDto,
+} from './dto/login.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ResponseDto } from 'src/common/dto/base-response.dto';
 
 @Controller('auth')
@@ -11,8 +15,19 @@ export class AuthController {
 
   @Post('login')
   @ApiOperation({ summary: '로그인 API' })
+  @ApiResponse({
+    status: 200,
+    description: '유저 정보가 이미 DB에 존재하고, 로그인에 성공',
+    type: LoginResponseDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: '유저 정보가 DB에 존재하지 않음',
+    type: NotExistUserResponseDto,
+  })
   async login(@Body() data: LoginRequestDto): Promise<ResponseDto> {
     const result = await this.authService.login(data);
+    console.log(result);
     return new ResponseDto(200, '성공', result);
   }
 }
--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -19,3 +19,38 @@ export class LoginRequestDto {
   @IsIn(['kakao', 'phone'])
   loginType: string;
 }
+
+export class LoginResponseDto {
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+    description: 'JWT 토큰',
+  })
+  @IsNotEmpty()
+  @IsString()
+  accessToken: string;
+}
+
+// DB에 존재하지 않을 때
+export class NotExistUserResponseDto {
+  @ApiProperty({
+    example: '장동호',
+    description: '유저 닉네임',
+  })
+  @IsString()
+  nickname?: string;
+
+  @ApiProperty({
+    example:
+      'http://k.kakaocdn.net/dn/zRtbv/btsr0t2vJzM/lgslaKS2K018lkL4KkBW9K/m1.jpg',
+    description: '유저 프로필 이미지',
+  })
+  @IsString()
+  profileImage?: string;
+
+  @ApiProperty({
+    example: '남성',
+    description: '유저 성별',
+  })
+  @IsString()
+  gender?: string;
+}
