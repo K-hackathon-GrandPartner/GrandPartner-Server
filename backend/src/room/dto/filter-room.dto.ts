a21a874e9fe4cef624935999113974c3da4f9f92
Jang-DongHo <donghoo18@naver.com>
2023-10-15 11:37:30 +0900

    [Fix] 허용 지역 추가

--- backend/src/room/dto/filter-room.dto.ts
+++ backend/src/room/dto/filter-room.dto.ts
@@ -2,7 +2,14 @@ import { ApiHeader, ApiProperty, ApiQuery } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
 import { IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
 
-const allowedRegions = ['광진구', '노원구', '성북구']; // 허용할 지역 목록
+const allowedRegions = [
+  '광진구',
+  '노원구',
+  '성북구',
+  '동대문구',
+  '서대문구',
+  '종로구',
+]; // 허용할 지역 목록
 const allowedBuildingTypes = ['아파트', '오피스텔', '빌라', '단독 주택']; // 허용할 건물 유형 목록
 const allowedRoomSizeTypes = ['소형', '중형', '대형', '대형+']; // 허용할 방 유형 목록
 const allowedRoomOptions = [
